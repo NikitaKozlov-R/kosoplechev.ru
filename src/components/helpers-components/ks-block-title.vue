519480fb95309c9f604b94ac876a7c8f99cb8532
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-09-19 03:50:19 +0300

    add tite component

---
+++ src/components/helpers-components/ks-block-title.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ks-block__title">
+    <h1 class="ks-block__title-text">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
+
+<style>
+.ks-block__title {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 8rem 0;
+}
+.ks-block__title-text {
+  font-size: 7rem;
+  text-align: center;
+}
+</style>
