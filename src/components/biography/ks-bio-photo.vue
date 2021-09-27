f8cc324ea3f1768265f85b5b6104c276e7295a2c
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-09-28 00:25:06 +0300

    added ks-bio-photo component & fill biography page

---
+++ src/components/biography/ks-bio-photo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ks-bio__bphoto">
+    <img class="ks-bio__bphoto-img" :src="url" :alt="alt" :title="alt" draggable="false" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      required: true,
+      type: String,
+    },
+    alt: {
+      required: true,
+      type: String,
+    },
+  },
+  data() {
+    return {
+      url: require('@/assets/img/' + this.imgUrl),
+    }
+  },
+}
+</script>
+
+<style>
+.ks-bio__bphoto {
+  width: 100%;
+  margin-top: 56px;
+}
+.ks-bio__bphoto-img {
+  width: 100%;
+  border-radius: 4px;
+}
+</style>
