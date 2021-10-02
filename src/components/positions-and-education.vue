e3f8cc22e2ae0c77947a8da9b174ef35cb53d404
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-02 21:27:50 +0300

    redesign positions-and-education component

--- src/components/positions-and-education.vue
+++ src/components/positions-and-education.vue
@@ -2,49 +2,57 @@
   <ks-block-title title="Образование и опыт" />
   <div class="ks-experience">
     <div class="ks-experience__col" data-aos="fade-up-right" data-aos-duration="2000">
-      <p class="ks-experience__col-timeline">2005 - 2010</p>
-      <div class="ks-experience__col-item">
-        <h4 class="ks-experience__col-header">
-          Московский государственный университет экономики, статистики и информатики
-        </h4>
-        <p class="ks-experience__col-position">Управление персоналом/Развитие человеческих ресурсов</p>
-      </div>
+      <p class="ks-experience__col-timeline">ОПЫТ РАБОТЫ: 2005 - 2010</p>
       <div class="ks-experience__col-item">
         <h4 class="ks-experience__col-header">ВЗАО "АСЭН"</h4>
         <p class="ks-experience__col-position">Начальник информационно-аналитического отдела</p>
       </div>
     </div>
     <div class="ks-experience__col" data-aos="fade-up" data-aos-duration="2000">
-      <p class="ks-experience__col-timeline">2005 - 2016</p>
+      <p class="ks-experience__col-timeline">2010 - 2016</p>
       <div class="ks-experience__col-item">
         <h4 class="ks-experience__col-header">Российский государственный социальный университет</h4>
         <p class="ks-experience__col-position">
           Заместитель декана факультета по работе со студентами, персоналом и организации практики
-        </p>
-        <p class="ks-experience__col-position">Социального управления и социологии, Туризм/Магистр</p>
-        <p class="ks-experience__col-position">
-          Лауреат общегородского конкурса «Молодой преподаватель ВУЗа» Московской городской организации Профсоюза
-          работников народного образования и науки РФ в номинации «Лучшее эссе» в 2016 г.
         </p>
       </div>
     </div>
     <div class="ks-experience__col" data-aos="fade-up-left" data-aos-duration="2000">
       <p class="ks-experience__col-timeline">2016 - ПО НАСТОЯЩЕЕ ВРЕМЯ</p>
       <div class="ks-experience__col-item">
-        <h4 class="ks-experience__col-header">Российский государственный социальный университет</h4>
+        <h4 class="ks-experience__col-header">Региональный эксперт Союза «Молодые профессионалы (Ворлдскиллс Россия)»</h4>
+        <p class="ks-experience__col-position">Союз «Молодые профессионалы (Ворлдскиллс Россия)», Администрирование отеля</p>
         <p class="ks-experience__col-position">Директор Колледжа РГСУ</p>
         <p class="ks-experience__col-position">Руководитель подразделения, преподаватель</p>
+      </div>
+    </div>
+  </div>
+  <div class="ks-experience">
+    <div class="ks-experience__col" data-aos="fade-up-right" data-aos-duration="2000">
+      <p class="ks-experience__col-timeline">ОБРАЗОВАНИЕ: 2005 - 2010</p>
+      <div class="ks-experience__col-item">
+        <h4 class="ks-experience__col-header">Московский государственный университет экономики, статистики и информатики</h4>
+        <p class="ks-experience__col-position">Управление персоналом/Развитие человеческих ресурсов</p>
+      </div>
+    </div>
+    <div class="ks-experience__col" data-aos="fade-up" data-aos-duration="2000">
+      <p class="ks-experience__col-timeline">2010 - 2016</p>
+      <div class="ks-experience__col-item">
+        <h4 class="ks-experience__col-header">Российский государственный социальный университет</h4>
+        <p class="ks-experience__col-position">Социального управления и социологии, Туризм/Магистр</p>
         <p class="ks-experience__col-position">
-          Региональный эксперт союза Worldskills по компетенции "Администрирование отеля", эксперт финала I
-          межвузовского чемпионата, главный эксперт отборочного этапа РГСУ
+          Лауреат общегородского конкурса «Молодой преподаватель ВУЗа» Московской городской организации Профсоюза работников
+          народного образования и науки РФ в номинации «Лучшее эссе» в 2016 г.
         </p>
       </div>
+    </div>
+    <div class="ks-experience__col" data-aos="fade-up-left" data-aos-duration="2000">
+      <p class="ks-experience__col-timeline">2016 - ПО НАСТОЯЩЕЕ ВРЕМЯ</p>
       <div class="ks-experience__col-item">
-        <h4 class="ks-experience__col-header">
-          Региональный эксперт Союза «Молодые профессионалы (Ворлдскиллс Россия)»
-        </h4>
+        <h4 class="ks-experience__col-header">WorldSkills Russia</h4>
         <p class="ks-experience__col-position">
-          Союз «Молодые профессионалы (Ворлдскиллс Россия)», Администрирование отеля
+          Региональный эксперт союза WorldSkills по компетенции "Администрирование отеля", эксперт финала I межвузовского
+          чемпионата, главный эксперт отборочного этапа РГСУ
         </p>
       </div>
     </div>
